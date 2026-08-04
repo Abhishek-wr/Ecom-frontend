2aa9a7b321dbce8d72082116f773af4d47588b6e
Abhishek-wr <Abhishek904175@gmail.com>
2026-08-04 14:53:05 +0530

    Initial commit with project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { FaBeer } from "react-icons/fa";
+
+function App(){
+  return(
+    <div className="flex items-center justify-center h-screen bg-gray-700 
+                    text-2xl font-bold text-white">
+      <h1>Abhishek <FaBeer /></h1>
+    </div>
+
+  )
+}
+export default App;
